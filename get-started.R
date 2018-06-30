f2c6c83de01f67b9a937471d5699cac6fbc2a1dd
maurolpeore <maurolepore@gmail.com>
2018-06-30 19:19:11 +0000

    Initial commit

---
+++ get-started.R
@@ -0,0 +1,7 @@
+# In rstudio.cloud, expect and disregard this warning:
+# Warning in fun(libname, pkgname) : couldn't connect to display ":0"
+library(fgeo)
+
+# Here is some code you may copy and paste in the console:
+# * Examples https://forestgeo.github.io/fgeo/#example
+# * Get started https://forestgeo.github.io/fgeo/articles/fgeo.html

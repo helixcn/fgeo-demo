9c46025f684cb68b37d098a07f35688bb8801902
maurolpeore <maurolepore@gmail.com>
2018-12-19 12:37:49 +0000

    Update get-started.

--- get-started.R
+++ get-started.R
@@ -1,3 +1,5 @@
 # Try fgeo by coping and pasting some code:
-# * Example: https://forestgeo.github.io/fgeo/index.html#example
-# * Get started: https://forestgeo.github.io/fgeo/articles/fgeo
+# * Example:
+browseURL("https://forestgeo.github.io/fgeo/index.html#example")
+# * Get started: 
+browseURL("https://forestgeo.github.io/fgeo/articles/siteonly/fgeo.html")
